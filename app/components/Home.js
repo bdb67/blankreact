bddf27dfc5012711692996d77f59cd7f11ecce5e
Brandon Bass <bdb67@cornell.edu>
2016-05-19 21:19:28 -0500

    first commit

---
+++ app/components/Home.js
@@ -0,0 +1,9 @@
+var React = require('react');
+
+function Home(){
+  return(
+    <h1> Home </h1>
+    )
+}
+
+module.exports = Home;
---
+++ app/components/Main.js
@@ -0,0 +1,9 @@
+var React = require('react');
+
+function Main(){
+  return(
+    <h1> Main </h1>
+    )
+}
+
+module.exports = Main;
